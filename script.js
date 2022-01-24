5c81250b069b9a93ac7f817dc7c9a65ce49fcc03
Aisalkyn <aisalkyn.4160@gmail.com>
2022-01-24 22:34:50 +0600

    Залила новый проект

---
+++ script.js
@@ -0,0 +1,6 @@
+const basket = document.querySelector('.basket');
+
+basket.onclick = () => {
+    const dropdown_menu2 = document.querySelector('.dropdown_menu2');
+    dropdown_menu2.classList.toggle('show_basket')
+}
