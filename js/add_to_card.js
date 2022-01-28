0d209bff1bbcc9fa2360ac308939b2abd7e63262
Aisalkyn <aisalkyn.4160@gmail.com>
2022-01-29 00:15:12 +0600

    'lesson_28'

---
+++ js/add_to_card.js
@@ -0,0 +1,6 @@
+let productImg = document.getElementsByClassName('img_good');
+console.log(productImg[0]);
+let productText = document.getElementsByClassName('ttl');
+console.log(productText[1]);
+let productPrice = document.getElementsByClassName('price');
+console.log(productPrice);
