fdeee7fa5bbcf853fbc19434e0fbf5634c36d36a
Aisalkyn <aisalkyn.4160@gmail.com>
2022-02-16 22:21:44 +0600

    dropdown_menu2

--- js/add_to_card.js
+++ js/add_to_card.js
@@ -9,18 +9,64 @@ productPrice[0].innerHTML = `<div class="price"><span>$12</span><del>$42</del></
 let elements = document.getElementsByClassName('add_cart');
 console.log(elements);
 
-function myFunction(){
-    var attribute = this.parentNode.parentNode.parentNode.querySelectorAll('.ttl');
-    const getPrice = this.parentNode.parentNode.parentNode.querySelectorAll('.price span');
-    const getUrlImg = this.parentNode.parentNode.parentNode.parentNode.querySelectorAll('.img_good');
-    const showBasket = `${attribute[0].innerText}/${getPrice[0].innerText}/${getUrlImg[0].getAttribute('src')}`;
+// function myFunction(){
+//     var attribute = this.parentNode.parentNode.parentNode.querySelectorAll('.ttl');
+//     const getPrice = this.parentNode.parentNode.parentNode.querySelectorAll('.price span');
+//     const getUrlImg = this.parentNode.parentNode.parentNode.parentNode.querySelectorAll('.img_good');
+//     const showBasket = `${attribute[0].innerText}/${getPrice[0].innerText}/${getUrlImg[0].getAttribute('src')}`;
 
-    alert(showBasket);
+//     alert(showBasket);
     
-};
+// };
 
-for (let i=0; i<elements.length; i++){
-    elements[i].addEventListener('click', myFunction, false);
-};
+// for (let i=0; i<elements.length; i++){
+//     elements[i].addEventListener('click', myFunction, false);
+// };
 
 
+// Добавление товара в корзину
+// function myFunction(){
+//     document.getElementById("myCheck").click();
+// }
+
+let cN =document.getElementsByClassName('add_cart');
+console.log(cN);
+
+let myFunction = function(evt){
+    evt.preventDefault();
+    let attribute = this.parentNode.parentNode.parentNode.querySelectorAll('.ttl');
+    let getPrice = this.parentNode.parentNode.parentNode.querySelectorAll('.price span');
+    let getUrlImg = this.parentNode.parentNode.parentNode.parentNode.querySelectorAll('.img_good');
+
+    let create_li = document.createElement('li')
+
+    console.log();
+    console.log();
+    console.log();
+
+    create_li.innerHTML = ' <div class="bag_img">\
+                                            <a href="?search=#">\
+                                                <img src="'+getUrlImg[0].src+'">\
+                                            </a>\
+                                        </div>\
+                                        <div class="bag_info">\
+                                            <p class="info_title"> <a href="?search=#">'+attribute[0].innerText+'</a> </p>\
+                                            <div class="info_price">\
+                                                <span>'+getPrice[0].innerText+'</span>\
+                                                <del>$42.00</del>\
+                                            </div>\
+                                            <div class="trash">\
+                                                <a href="?search=#"><i class="fas fa-trash"></i></a>\
+                                            </div>\
+                                        </div>';
+                            
+    let block_cart = document.querySelector('.dropdown_menu2 ul');
+    block_cart.appendChild(create_li);
+    
+    alert('Товары успешно добавлены в корзину');
+    
+}
+
+for (let i=0; i< cN.length; i++){
+    cN[i].addEventListener('click', myFunction,false);
+}
